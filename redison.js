d84c55bd1d0dbd8a5feb851042d104e7cb6440e0
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-03-08 20:14:45 +0530

    updated

--- redison.js
+++ redison.js
@@ -1,6 +1,7 @@
 const config = require("@bootloader/config");
 const IORedis = require("ioredis");
 const MockIORedis = require("./MockIORedis");
+const { requireGlobal, system } = require("@bootloader/utils");
 
 const redisHost =
   config.getIfPresent("redis.host") || config.getIfPresent("mry.redis.host");
@@ -13,36 +14,55 @@ const isRedisMock = redisHost == "<host>" || !redisHost;
 
 async function startRedisServer() {
   try {
-    const RedisServer = require("redis-server");
+    const RedisServer = requireGlobal("redis-server");
+    console.error("✅ package(redis-server) found!");
     let server = new RedisServer(6379);
     await server.open();
-    console.error("✅ package(redis-server) found!");
+    console.error("✅ package(redis-server) connected!");
     return { host: server.host, port: server.port };
-  } catch (err1) {
+  } catch (error) {
     console.info("❌ package(redis-server) not found!");
-    try {
-      const { RedisMemoryServer } = require("redis-memory-server");
-      let redisServer = await RedisMemoryServer.create(); // Create in-memory Redis
-      console.error("✅ package(redis-memory-server) found!");
-      return {
-        host: await redisServer.getHost(),
-        port: await redisServer.getPort(),
-      };
-    } catch (err2) {
-      console.error("❌ No in-memory Redis package found!");
-      console.error(
-        "💡 Install either `redis-server` or `redis-memory-server` globally:"
-      );
-      console.error("npm install -g redis-server");
-      console.error("OR (only for Non-Windows)");
-      console.error("npm install -g redis-memory-server");
+    throw error;
+  }
+}
+
+async function startRedisInMemoryServer() {
+  try {
+    const { RedisMemoryServer } = requireGlobal("redis-memory-server");
+    console.error("✅ package(redis-memory-server) found!");
+    let redisServer = await RedisMemoryServer.create(); // Create in-memory Redis
+    console.error("✅ package(redis-memory-server) connected!");
+    return {
+      host: await redisServer.getHost(),
+      port: await redisServer.getPort(),
+    };
+  } catch (error) {
+    console.error("❌ No in-memory Redis package found!");
+    return await startRedisServer();
+  }
+}
+
+async function startRedisLocalServer() {
+  try {
+    if (system.isWindows()) {
+      return await startRedisServer();
+    } else {
+      return await startRedisInMemoryServer();
     }
+  } catch (err1) {
+    console.error(
+      "💡 Install either `redis-server` or `redis-memory-server` globally:"
+    );
+    console.error("(for Linux/Mac)");
+    console.error("npm install -g redis-memory-server");
+    console.error("OR (only for Non-Windows)");
+    console.error("npm install -g redis-server");
   }
 }
 
 async function connectMock(IORedisClient) {
-  const { host, port } = await startRedisServer(); // Create in-memory Redis
-  console.log(`Connecting to in-memory Redis`);
+  console.log(`⚠️ Connecting to in-memory Mock Redis`);
+  const { host, port } = await startRedisLocalServer(); // Create in-memory Redis
   await IORedisClient.connect({ host, port });
 }
 
@@ -85,7 +105,7 @@ const client = (function (host) {
 client.on("error", (err) => console.error("Redis Client Error", err));
 client.on("connect", () => console.log("Redis Client connected"));
 if (typeof client.connect == "function") {
-  console.log("=====connecting");
+  //console.log("=====connecting");
   if (redisClient == "redis") {
     client.connect();
   }
